f53b59c114c60b47c7a773953d7464b5194d9d8d
Shreya Patidar <shreyapatidar2006@gmail.com>
2025-11-22 11:16:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
   alert("Message Sent Successfully!");
 });
+//drag and drop
+document.querySelectorAll(".subject-box").forEach(box => {
+    box.addEventListener("click", () => {
+        const targetId = box.getAttribute("data-target");
+        const panel = document.getElementById(targetId);
+
+        panel.style.display = (panel.style.display === "block") ? "none" : "block";
+    });
+});
+//achivment section
+function toggleAchievement(id) {
+    const box = document.getElementById(id).closest(".subject-box");
+    const content = document.getElementById(id);
+
+    // toggle background color white/purple
+    box.classList.toggle("open");
+
+    // show/hide achievements
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
